29b3b2a0b7835a45b741e7758a4860df47b766cd
Joanna Lin <linjoanna31@gmail.com>
2022-09-12 16:20:54 +1000

    Added week 1 content

---
+++ tut01/cat.c
@@ -0,0 +1,10 @@
+// A program that prints a cat face
+// Joanna, September 2022
+
+#include <stdio.h>
+
+int main(void) {
+    printf(" /\\___/\\\n");
+    printf("(=' w '=)\n");
+    return 0;
+}
---
+++ tut01/hello_world.c
@@ -0,0 +1,10 @@
+// Basic Hello World program
+// Joanna, September 2022
+
+#include <stdio.h>
+
+int main(void) {
+    printf("Hello World\n");
+    
+    return 0;
+}
